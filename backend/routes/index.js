417356e78ab5d3e2ca7d0d3e33ade86c7e58929d
Kyle Parkin <13ktparkin@gmail.com>
2023-01-19 14:06:46 -0500

    added a api router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,13 +1,16 @@
-  // backend/routes/index.js
-  const express = require('express');
-  const router = express.Router();
-  
-  router.get("/api/csrf/restore", (req, res) => {
-    const csrfToken = req.csrfToken();
-    res.cookie("XSRF-TOKEN", csrfToken);
-    res.status(200).json({
-      'XSRF-Token': csrfToken
-    });
+// backend/routes/index.js
+const express = require("express");
+const router = express.Router();
+const apiRouter = require("./api");
+
+router.use("/api", apiRouter);
+
+router.get("/api/csrf/restore", (req, res) => {
+  const csrfToken = req.csrfToken();
+  res.cookie("XSRF-TOKEN", csrfToken);
+  res.status(200).json({
+    "XSRF-Token": csrfToken,
   });
-  
-  module.exports = router;
+});
+
+module.exports = router;
