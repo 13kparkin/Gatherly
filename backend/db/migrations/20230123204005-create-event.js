ea3630781cc4cc15bbc5722b8d79e79fb6bd4448
Kyle Parkin <13ktparkin@gmail.com>
2023-01-23 16:58:06 -0500

    created the table groups and seeded it

--- backend/db/migrations/20230123204005-create-event.js
+++ backend/db/migrations/20230123204005-create-event.js
@@ -43,10 +43,12 @@ module.exports = {
         allowNull: false,
       },
       createdAt: {
+        allowNull: false,
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       },
       updatedAt: {
+        allowNull: false,
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
---
+++ backend/db/seeders/20230123214815-demo-group.js
@@ -0,0 +1,42 @@
+'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define your schema in options object
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    options.tableName = "Groups";
+    return queryInterface.bulkInsert(options, [{
+      organizerId: 1,
+      name: 'demo-group-1',
+      about: 'This is the first demo group',
+      type: 'Online',
+      private: false,
+      city: 'New York',
+      state: 'NY',
+  }, {
+      organizerId: 2,
+      name: 'demo-group-1',
+      about: 'This is the second demo group',
+      type: 'Online',
+      private: true,
+      city: 'Los Angeles',
+      state: 'CA',
+  }], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    options.tableName = "Groups";
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(
+      options,
+      {
+        name: { [Op.in]: ["demo-group-1", "demo-group-2"] },
+      },
+      {}
+    );
+  }
+};
