901997b0271b4f560e6ea2c8e28819889a30e8d2
Kyle Parkin <13ktparkin@gmail.com>
2023-02-09 17:17:46 -0500

    finished adding a slice of state

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,8 +6,7 @@ import './index.css';
 import App from './App';
 import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
-
-
+import * as sessionActions from './store/session';
 
 const store = configureStore();
 
@@ -16,7 +15,9 @@ if (process.env.NODE_ENV !== 'production') {
 
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
+
 
 
 function Root() {
