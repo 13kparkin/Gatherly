9a80c6fae3c1ca7335a4f57229002461ad80e23b
Kyle Parkin <13ktparkin@gmail.com>
2023-02-09 19:03:49 -0500

    finished modal login

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -3,6 +3,8 @@ import { NavLink } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import * as sessionActions from '../../store/session';
+import OpenModalButton from '../OpenModalButton';
+import LoginFormModal from '../LoginFormModal';
 import './Navigation.css';
 
 function Navigation({ isLoaded }){
@@ -25,9 +27,12 @@ function Navigation({ isLoaded }){
   } else {
     sessionLinks = (
       <li>
-        <NavLink to="/login">Log In</NavLink>
-        <NavLink to="/signup">Sign Up</NavLink>
-      </li>
+      <OpenModalButton
+        buttonText="Log In"
+        modalComponent={<LoginFormModal />}
+      />
+      <NavLink to="/signup">Sign Up</NavLink>
+    </li>
     );
   }
 
