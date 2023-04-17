a237062fad53f1d57e0da6e1ffde59ea3e0585ee
Kyle Parkin <13ktparkin@gmail.com>
2023-04-17 22:12:25 +0100

    fixed seed data

--- backend/db/seeders/20230123221503-demo-events.js
+++ backend/db/seeders/20230123221503-demo-events.js
@@ -25,7 +25,7 @@ module.exports = {
           endDate: moment().add(2, 'months').startOf('day').add(2, 'days').hour(23).minute(0).second(0).format('YYYY-MM-DD HH:mm:ss')
         },
         {
-          venueId: 2,
+          venueId: 1,
           groupId: 2,
           name: "Tech Conference 2023",
           description: "The must-attend conference for tech enthusiasts",
@@ -36,7 +36,7 @@ module.exports = {
           endDate: moment().add(3, 'months').startOf('day').add(3, 'days').hour(17).minute(0).second(0).format('YYYY-MM-DD HH:mm:ss')
         },
         {
-          venueId: 3,
+          venueId: 1,
           groupId: 3,
           name: "Virtual Art Exhibition",
           description: "Experience art from around the world from the comfort of your own home",
@@ -47,8 +47,8 @@ module.exports = {
           endDate: moment().add(1, 'month').startOf('day').add(1, 'week').hour(18).minute(0).second(0).format('YYYY-MM-DD HH:mm:ss')
         },
         {
-          venueId: 4,
-          groupId: 4,
+          venueId: 1,
+          groupId: 3,
           name: "Wine Tasting and Pairing",
           description: "Learn about different wines and food pairings with our expert sommeliers",
           type: "in person",
@@ -58,8 +58,8 @@ module.exports = {
           endDate: moment().add(4, 'months').startOf('day').add(1, 'days').hour(22).minute(0).second(0).format('YYYY-MM-DD HH:mm:ss')
         },
         {
-          venueId: 5,
-          groupId: 5,
+          venueId: 1,
+          groupId: 2,
           name: "Entrepreneurship Summit",
           description: "Connect with other entrepreneurs and learn about the latest trends and strategies",
           type: "in person",
